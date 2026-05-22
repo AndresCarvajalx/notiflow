e07f54e536ef7d49ba4957c729b23a492c0b26e0
AndresCarvajalx <andresfelipecarvajalduran3@gmail.com>
2026-05-22 15:22:11 -0500

    feat: refactor logger initialization to use timestamped log files and clean old logs

--- logger/logger.go
+++ logger/logger.go
@@ -1,17 +1,22 @@
 package logger
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var L *zap.Logger
 
 func Init() {
 	os.MkdirAll("./logs", 0755)
+	cleanOldLogs(30)
+
+	filename := fmt.Sprintf("./logs/notiflow-%s.log", time.Now().Format("2006-01-02"))
 
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.TimeKey = "time"
@@ -19,14 +24,12 @@ func Init() {
 
 	encoder := zapcore.NewJSONEncoder(encoderConfig)
 
-	fileWriter := zapcore.AddSync(&lumberjack.Logger{
-		Filename:   "./logs/notiflow.log",
-		MaxSize:    10,
-		MaxBackups: 30,
-		MaxAge:     30,
-		Compress:   true,
-	})
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
 
+	fileWriter := zapcore.AddSync(file)
 	consoleWriter := zapcore.AddSync(os.Stdout)
 
 	fileCore := zapcore.NewCore(
@@ -47,4 +50,21 @@ func Init() {
 	)
 
 	L = zap.New(core, zap.AddCaller())
+}
+
+func cleanOldLogs(maxDays int) {
+	files, err := filepath.Glob("./logs/notiflow-*.log")
+	if err != nil {
+		return
+	}
+	threshold := time.Now().AddDate(0, 0, -maxDays)
+	for _, f := range files {
+		info, err := os.Stat(f)
+		if err != nil {
+			continue
+		}
+		if info.ModTime().Before(threshold) {
+			os.Remove(f)
+		}
+	}
 }
